1c5a04c6347de4abc3c1f0e8b3b3f9a700c66b49
Amit <amitsinha029384@gmail.com>
2025-05-04 10:17:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: `DialogContent` requires a `DialogTitle` for the component to be accessible for screen reader users.
    >
    > If you want to hide the `DialogTitle`, you can wrap it with our VisuallyHidden component.
    >
    > For more information, see https://radix-ui.com/primitives/docs/components/dialog. Error source: src/components/ui/sheet.tsx (62:5) @ _c1
    >
    >   60 |   <SheetPortal>
    >   61 |     <SheetOverlay />
    > > 62 |     <SheetPrimitive.Content
    >      |     ^
    >   63 |       ref={ref}
    >   64 |       className={cn(sheetVariants({ side }), className)}
    >   65 |       {...props}
    >
    > Call Stack
    > 27
    >
    > Show 23 ignore-listed frame(s)
    > _c1
    > src/components/ui/sheet.tsx (62:5)
    > _c1
    > src/components/ui/sheet.tsx (60:3)
    > Navbar
    > src/components/navigation/navbar.tsx (96:13)
    > RootLayout
    > src/app/layout.tsx (49:9)

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -64,6 +65,11 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
+      {/* Add a visually hidden title for accessibility */}
+      <SheetPrimitive.Title className="sr-only">
+         {/* Default title, can be overridden by SheetTitle if used */}
+         Navigation Menu
+      </SheetPrimitive.Title>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
